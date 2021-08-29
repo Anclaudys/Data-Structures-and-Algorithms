a6e8409a373dec2bfc7ce30accc18e72b0fd4f47
Anclaudys <64286678+Anclaudys@users.noreply.github.com>
2021-08-28 22:23:05 -0400

    add windows g++ powershell commands

--- config.py
+++ config.py
@@ -1,23 +1,29 @@
 import os
 import sys
 import platform
+import subprocess
+import time
 
 def os_config():
   info = platform.uname()
   if(info.system =="Windows"):
-    print("It's windows")
     win_cmd()
 
 def win_cmd():
   if sys.argv[1] == 'cpp':
-    os.system(f'cmd /c g++ tests/cpp/dropboxtest.cpp"')
-    os.system('cmd /c .\\a.exe')
+    os.system(f"cmd /c g++ tests/cpp/{sys.argv[2]}test.cpp -o tests/cpp/_run/{sys.argv[2]}test")
+    t = 3
+    while not os.path.isfile(f"tests/cpp/_run/{sys.argv[2]}test.exe"):
+      countdown(t)
+    testfile = os.path.join(f"tests//cpp/_run/{sys.argv[2]}test.exe")
+    subprocess.call([testfile])
 
-
-
-
-
-
-
+def countdown(t):
+  while t:
+    mins, secs = divmod(t, 60)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print("Compiling... ", timer, end="\r")
+    time.sleep(1)
+    t -= 1
 
 os_config()
